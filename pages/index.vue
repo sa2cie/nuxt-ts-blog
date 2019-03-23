034cff9c2c7d566638632768fbe97a5461407bf5
sa2cie <sa2cie.dev@gmail.com>
2019-03-23 21:47:39 +0900

    Update pages/index view sample component

--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,10 @@
 ************************************************************************* -->
 
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-ts-blog
-      </h1>
-      <h2 class="subtitle">
-        My groundbreaking Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
-  </section>
+  <div>
+    <logo />
+    <card />
+  </div>
 </template>
 
 <!-- *************************************************************************
