e961fdf8513803ad867dace7c8af8c5eda6523af
sa2cie <sa2cie.dev@gmail.com>
2019-03-23 22:00:58 +0900

    Update Card component

--- components/Card/index.vue
+++ components/Card/index.vue
@@ -9,7 +9,7 @@
         <v-img
           src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
           aspect-ratio="2.75"
-        ></v-img>
+        />
 
         <v-card-title primary-title>
           <div>
