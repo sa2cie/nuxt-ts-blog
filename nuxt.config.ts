1344675819c4aab64a03273950a71a2330fb2565
sa2cie <sa2cie.dev@gmail.com>
2019-03-23 21:39:58 +0900

    Add package vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,8 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    "@nuxtjs/vuetify"
   ],
   /*
    ** Axios module configuration
